b4bdda02b726e9038b8fa647c2b364d5fec8344d
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2025-04-14 19:27:35 -0600

    Cree un nuevo componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Tarjeta from './Tarjeta'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <h1>Hola clase como estan ?</h1>
+     <Tarjeta/>
     </>
   )
 }
---
+++ src/Tarjeta.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Tarjeta(){
+
+    return (
+        <div>
+            <h1>Hola soy la tarjeta</h1>
+        </div>
+    )
+}
+
+export default Tarjeta
